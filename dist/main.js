a300838a22002f919833dbe4aa4737d89a7d64d2
Reuben T Allik <reubenallik@gmail.com>
2021-09-13 11:52:25 -0400

    through arrow functions"

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,11 @@
+// variable type declaration
+// ---> ': TYPE' after variable name 
 var test = "hello world";
 console.log(test);
+//function arg/return declaration
+// ---> same convention as variables for args
+// ---> return fro arrow func uses ': TYPE' after parenthasis and before arrow
+var getFullName = function (firstname, lastname) {
+    return firstname + ' ' + lastname;
+};
+console.log(getFullName('George', 'Smith'));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-const test = "hello world"
+// variable type declaration
+// ---> ': TYPE' after variable name 
+let test: string = "hello world"
 console.log(test);
+
+//function arg/return declaration
+// ---> same convention as variables for args
+// ---> return fro arrow func uses ': TYPE' after parenthasis and before arrow
+const getFullName = (firstname: string, lastname: string): string => {
+    return firstname + ' ' + lastname;
+}
+
+console.log(getFullName('George', 'Smith'))
