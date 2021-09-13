9680df3d76b1eec4506a19f822ad2fef944f9a7a
Reuben T Allik <reubenallik@gmail.com>
2021-09-13 12:05:49 -0400

    more object interface work

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+/* VARIABLES AND FUNCTIONS */
 // variable type declaration
 // ---> ': TYPE' after variable name 
 var test = "hello world";
@@ -9,3 +10,19 @@ var getFullName = function (firstname, lastname) {
     return firstname + ' ' + lastname;
 };
 console.log(getFullName('George', 'Smith'));
+/* OBJECTS */
+var user = {
+    name: 'Joan',
+    age: 40
+};
+var customer = {
+    name: 'Doug',
+    age: 3
+};
+var mac2015 = {
+    brand: 'Apple',
+    seller_comments: "Great condition"
+};
+var pc2020 = {
+    brand: 'HP'
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* VARIABLES AND FUNCTIONS */
+
 // variable type declaration
 // ---> ': TYPE' after variable name 
 let test: string = "hello world"
@@ -11,3 +13,37 @@ const getFullName = (firstname: string, lastname: string): string => {
 }
 
 console.log(getFullName('George', 'Smith'))
+
+
+/* OBJECTS */
+const user: {name: string, age: number} = {
+    name: 'Joan',
+    age: 40
+}
+
+
+/* Interfaces */
+interface Customer {
+    name: string;
+    age: number;
+}
+
+const customer: Customer = {
+    name: 'Doug',
+    age: 3
+}
+
+/* Object non-mandatory properties */
+interface Computer {
+    brand: string,
+    seller_comments?: string
+}
+
+const mac2015: Computer = {
+    brand: 'Apple',
+    seller_comments: "Great condition"
+}
+
+const pc2020: Computer = {
+    brand: 'HP'
+}
